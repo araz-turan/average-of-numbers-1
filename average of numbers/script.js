5c8c1424df1b0d45e35bfa6b89577e2344cf5430
Araz <122231385+araz-turan@users.noreply.github.com>
2024-10-27 00:25:55 +0330

    average of numbers-1
    Takes three numbers as input and calculates their average

---
+++ average of numbers/script.js
@@ -0,0 +1,7 @@
+var entryNum1 = Number(prompt('Enter your first number: ', [0]));
+var entryNum2 = Number(prompt('Enter your second number: ', [0]));
+var entryNum3 = Number(prompt('Enter your third number: ', [0]));
+
+var average = (entryNum1 + entryNum2 + entryNum3) / 3;
+
+alert(average);
